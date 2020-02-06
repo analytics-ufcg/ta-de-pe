b6ebf69aaa63480191f96693e16e232426f66106
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-02-06 14:04:42 -0300

    Adiciona get de orgaos

--- server/routes/api/orgaos.js
+++ server/routes/api/orgaos.js
@@ -4,14 +4,23 @@ const Sequelize = require("sequelize");
 const router = express.Router();
 
 const models = require("../../models/index");
-const Orgao = models.orgao;
+const Orgaos = models.orgao;
 
 const BAD_REQUEST = 400;
 const SUCCESS = 200;
 
+router.get("/", (req, res) => {
+
+    Orgaos.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('nm_orgao')), 'nm_orgao']]
+    })
+    .then(municipios => res.status(SUCCESS).json(municipios))
+    .catch(err => res.status(BAD_REQUEST).json({ err }));
+})
+
 router.get("/municipios", (req, res) => {
 
-    Orgao.findAll({
+    Orgaos.findAll({
         attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('nome_municipio')), 'nome_municipio']]
     })
     .then(municipios => res.status(SUCCESS).json(municipios))
