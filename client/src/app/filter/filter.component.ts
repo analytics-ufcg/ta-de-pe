387cb07b8749a85540d7df35a19c5da868ea4432
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2020-03-04 16:16:44 -0300

    inicia componente de filtro

---
+++ client/src/app/filter/filter.component.ts
@@ -0,0 +1,94 @@
+import { Component, EventEmitter, OnInit, Output, OnDestroy, Input } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Licitacao } from '../shared/models/licitacao.model';
+import { LicitacaoService } from '../shared/services/licitacao.service';
+
+@Component({
+    selector: 'app-filter',
+    templateUrl: './filter.component.html',
+    styleUrls: ['./filter.component.scss']
+})
+
+export class FilterComponent implements OnDestroy {
+
+    @Output() filterChange = new EventEmitter<any>();
+
+    filtro: any;
+    private unsubscribe = new Subject();
+
+    licitacaoSelecionada: string;
+    // evoluir para:
+    contratoSelecionado: string;
+    empenhoSelecionado: string;
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private modalService: NgbModal,
+        private licitacaoService: LicitacaoService
+    ) {
+        this.licitacaoSelecionada = 'Sim';
+        this.empenhoSelecionado = 'Sim';
+
+        this.filtro = {
+            licitacao: this.licitacaoSelecionada,
+            default: true
+        };
+    }
+
+    aplicarFiltro() {
+        this.filtro = {
+            licitacao: this.licitacaoSelecionada === 'Sim' ? true : false,
+            empenho: this.empenhoSelecionado === 'Sim' ? true : false,
+
+        };
+        this.updateUrlFiltro(this.filtro);
+
+        this.filterChange.emit(this.filtro);
+        this.modalService.dismissAll();
+    }
+
+    limparFiltro() {
+        this.licitacaoSelecionada = 'Sim';
+        this.empenhoSelecionado = 'Sim';
+        this.aplicarFiltro();
+    }
+
+    limparFiltroLicitacao() {
+        this.licitacaoSelecionada = 'Sim';
+        this.aplicarFiltro();
+    }
+
+    limparFiltroEmpenho() {
+        this.empenhoSelecionado = 'Sim';
+        this.aplicarFiltro();
+    }
+
+    private updateUrlFiltro(filtro: any) {
+        const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
+
+        if (filtro.licitacao !== 'Sim') {
+            queryParams.licitacao = filtro.licitacao;
+        } else {
+            delete queryParams.licitacao;
+        }
+
+        if (filtro.empenho !== 'Sim') {
+            queryParams.empenho = filtro.empenho;
+        } else {
+            delete queryParams.empenho;
+        }
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
+
+
+}
