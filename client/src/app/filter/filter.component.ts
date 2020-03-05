547cb484bbffa4a84224bbd6b514cc825fd46ebe
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2020-03-05 14:53:55 -0300

    adiciona empenho selecionado

--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -35,6 +35,7 @@ export class FilterComponent implements OnDestroy {
 
         this.filtro = {
             licitacao: this.licitacaoSelecionada,
+            empenho: this.empenhoSelecionado,
             default: true
         };
     }
@@ -49,6 +50,7 @@ export class FilterComponent implements OnDestroy {
             empenho: this.empenhoSelecionado === 'Sim' ? true : false,
 
         };
+        
         this.updateUrlFiltro(this.filtro);
 
         this.filterChange.emit(this.filtro);
