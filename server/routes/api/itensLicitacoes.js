bab8ac509d6923a2743084b01e0e3e3bf147651c
gileadekelvin <gileadekelvin@gmail.com>
2020-03-18 19:12:41 -0300

    Adiciona endpoint para recuperar todos os itens de uma licitação

--- server/routes/api/itensLicitacoes.js
+++ server/routes/api/itensLicitacoes.js
@@ -9,9 +9,21 @@ const itensLicitacao = models.itensLicitacao;
 const BAD_REQUEST = 400;
 const SUCCESS = 200;
 
+// Recupera todos os itens da licitação
 router.get("/", (req, res) => {
   itensLicitacao.findAll()
     .then(itensLicitacoes => res.status(SUCCESS).json(itensLicitacoes))
+    .catch(err => res.status(BAD_REQUEST).json({ err }));
+});
+
+// Recupera itens da licitação a partir do id da licitação
+router.get("/licitacao/:id", (req, res) => {
+  itensLicitacao.findAll({
+    where: {
+      id_licitacao: req.params.id
+    }
+  })
+    .then(itensLicitacao => res.status(SUCCESS).json(itensLicitacao))
     .catch(err => res.status(BAD_REQUEST).json({ err }));
 });
 
