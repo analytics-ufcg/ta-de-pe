2a88a051fa2bd056dc2638658377a6c1a625ad1d
Jefferson Neves <jefferson.rpn@gmail.com>
2020-03-27 16:07:14 -0300

    Enriquece textos de novidades

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CurrencyPipe } from '@angular/common';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +24,7 @@ import { NovidadeService } from './shared/services/novidade.service';
     NgbModule
   ],
   providers: [
+    CurrencyPipe,
     UserService,
     NovidadeService
   ],
