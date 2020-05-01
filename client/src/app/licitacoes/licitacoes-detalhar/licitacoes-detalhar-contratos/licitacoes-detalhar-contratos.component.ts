40df0bc5c4ddb725b432b98cf1e6b6f52dd60397
Jefferson Neves <jefferson.rpn@gmail.com>
2020-04-30 23:53:40 -0300

    Acrescenta diferença entre valor total contratado e média total do Estado

--- client/src/app/licitacoes/licitacoes-detalhar/licitacoes-detalhar-contratos/licitacoes-detalhar-contratos.component.ts
+++ client/src/app/licitacoes/licitacoes-detalhar/licitacoes-detalhar-contratos/licitacoes-detalhar-contratos.component.ts
@@ -49,13 +49,13 @@ export class LicitacoesDetalharContratosComponent implements OnInit, OnDestroy {
             return sum + (item.itensLicitacaoItensContrato.vl_unitario_estimado * item.qt_itens_contrato);
           }, 0);
           contrato.itensContrato.map(item => {
-            item.media_valor = item.itensSemelhantes.filter(d => {
-              return d.ano_licitacao === item.ano_licitacao;
-            }).reduce((sum, itemB) => {
-              return sum + itemB.vl_item_contrato / item.itensSemelhantes.filter(d => {
-                return d.ano_licitacao === item.ano_licitacao;
-              }).length;
-            }, 0);
+            item.media_valor = item.itensSemelhantes
+              .filter(d => d.ano_licitacao === item.ano_licitacao)
+              .reduce((sum, itemB) => {
+                return sum + itemB.vl_item_contrato / item.itensSemelhantes.filter(d => {
+                  return d.ano_licitacao === item.ano_licitacao;
+                }).length;
+              }, 0);
           });
         });
         this.isLoading = false;
