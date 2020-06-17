a74d2cd728b14cd98ccfad40ae604de0b0889858
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-06-17 18:56:56 -0300

    Cria versão inicial da tela de contrato

--- client/src/app/contratos/info-contrato/info-contrato.component.ts
+++ client/src/app/contratos/info-contrato/info-contrato.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import * as d3 from 'd3-scale';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { takeUntil, take, map } from 'rxjs/operators';
+
+import { ContratoLicitacao } from 'src/app/shared/models/contratoLicitacao.model';
+import { ItensContrato } from 'src/app/shared/models/itensContrato.model';
+import { ContratoService } from 'src/app/shared/services/contrato.service';
+import { ItensService } from 'src/app/shared/services/itens.service';
+
 
 @Component({
   selector: 'app-info-contrato',
@@ -7,9 +20,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoContratoComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe = new Subject();
+
+  public contrato: ContratoLicitacao;
+  public itemSelecionado: ItensContrato;
+  public isLoading = true;
+  public radioGroupForm: FormGroup;
+
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal,
+    private contratoService: ContratoService,
+    private itensService: ItensService
+  ) { }
 
   ngOnInit() {
+    this.activatedroute.params.pipe(take(1)).subscribe(params => {
+      this.getContratoByID(params.id);
+    });
+    this.radioGroupForm = this.formBuilder.group({
+      showTotal: false
+    });
+  }
+
+  getContratoByID(id: string) {
+    this.contratoService.get(id)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(contrato => {
+        this.contrato = contrato;
+        this.contrato.valor_contratado = contrato.itensContrato.reduce((sum, item) => {
+          return sum + (item.vl_item_contrato * item.qt_itens_contrato);
+        }, 0);
+        this.contrato.valor_estimado = contrato.itensContrato.reduce((sum, item) => {
+          return sum + (item.itensLicitacaoItensContrato.vl_unitario_estimado * item.qt_itens_contrato);
+        }, 0);
+        contrato.itensContrato.map(item => {
+          const tituloItem = item.ds_item.split(/\s+|:|-/).slice(0, 3).map(palavra => {
+            return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+          }).filter(i => i !== '');
+          this.getMediaItensSemelhantes(tituloItem, item.dt_inicio_vigencia)
+            .then(res => {
+              item.mediana_valor = res.mediana;
+              item.itensSemelhantes = res.itensOrdenados;
+              item.percentual_vs_estado = (item.vl_item_contrato - res.mediana) / res.mediana;
+              item.percentual_vs_estimado = (item.vl_item_contrato - item.itensLicitacaoItensContrato.vl_unitario_estimado)
+                                            / item.itensLicitacaoItensContrato.vl_unitario_estimado;
+            });
+        });
+        this.isLoading = false;
+      });
+  }
+
+  getMediaItensSemelhantes(dsItem: string[], dataInicioContrato: Date) {
+    const termos = [dsItem[0], dsItem.slice(0, 2).join(' & '), dsItem.join(' & ')];
+    return this.itensService.getItensSimilares(termos, dataInicioContrato)
+      .pipe(take(1),
+        map(itens => {
+          const itensOrdenados = itens.slice(0, 21).sort((a, b) => a.vl_item_contrato - b.vl_item_contrato);
+          const meioInf = Math.floor((itensOrdenados.length - 1) / 2);
+          const meioSup = Math.ceil((itensOrdenados.length - 1) / 2);
+          const mediana = (itensOrdenados[meioInf].vl_item_contrato + itensOrdenados[meioSup].vl_item_contrato) / 2;
+          return { mediana, itensOrdenados };
+        })
+      ).toPromise();
+  }
+
+  defineCorFundo(valor: number): string {
+    const cor: any = d3.scaleLinear()
+    .domain([-1, 0, 1])
+    .range(['#72a5b6', '#ffffff', '#d7856c']);
+    return cor(valor);
+  }
+
+  defineCor(valor: number): string {
+    return (valor >= 1 || valor <= -1) ? 'white' : 'black';
+  }
+
+  open(content, item: ItensContrato): void {
+    this.itemSelecionado = item;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-descricao', size: 'xl' });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
