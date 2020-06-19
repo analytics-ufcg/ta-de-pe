04287dcae9669454fdb61b31170ad12dd7eadf8a
Jefferson Neves <jefferson.rpn@gmail.com>
2020-06-18 23:24:16 -0300

    Ajusta pipe de formatar CPF/CNPJ para valores nulos

--- client/src/app/shared/pipes/format-cpf-cnpj.pipe.ts
+++ client/src/app/shared/pipes/format-cpf-cnpj.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatCpfCnpjPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
+    if (!value) { return value; }
     const cnpjCpf = value.replace(/\D/g, '');
 
     if (cnpjCpf.length === 11) {
