8b6e090aca0a1c71b227cb58923681b3de5bf5b0
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-06-30 14:55:59 -0300

    Implementa rota para página de item de contrato

--- client/src/app/itens/itens-routing.module.ts
+++ client/src/app/itens/itens-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { InfoItemComponent } from './info-item/info-item.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: ':id',
+    component: InfoItemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
