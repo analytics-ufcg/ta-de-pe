a63ee0bceb745cc1e65978badb9a0d7c89d91050
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-07-01 15:56:25 -0300

    Implementa versão estável da página do item

--- client/src/app/itens/itens.module.ts
+++ client/src/app/itens/itens.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ItensRoutingModule } from './itens-routing.module';
 import { InfoItemComponent } from './info-item/info-item.component';
+import { SharedModule } from '../shared/components/shared.module';
+import { PipesModule } from '../shared/pipes/pipes.module';
 
 
 @NgModule({
   declarations: [InfoItemComponent],
   imports: [
     CommonModule,
-    ItensRoutingModule
+    ItensRoutingModule,
+    SharedModule,
+    PipesModule
   ]
 })
 export class ItensModule { }
