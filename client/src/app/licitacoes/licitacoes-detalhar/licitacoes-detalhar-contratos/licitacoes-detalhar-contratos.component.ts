730cf9a6f47956b1a6aacd05fbf02f3850f8943b
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-07-23 11:10:51 -0300

    Conserta bug que não mostrava nomes de fornecedores em página da licitação

--- client/src/app/licitacoes/licitacoes-detalhar/licitacoes-detalhar-contratos/licitacoes-detalhar-contratos.component.ts
+++ client/src/app/licitacoes/licitacoes-detalhar/licitacoes-detalhar-contratos/licitacoes-detalhar-contratos.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
-import { take, map } from 'rxjs/operators';
+import { take, concatMap } from 'rxjs/operators';
 
 import { ContratoLicitacao } from 'src/app/shared/models/contratoLicitacao.model';
 import { LicitacaoService } from 'src/app/shared/services/licitacao.service';
-import { ItensContrato } from 'src/app/shared/models/itensContrato.model';
-import { ItensService } from 'src/app/shared/services/itens.service';
-
-import * as d3 from 'd3-scale';
 
 @Component({
   selector: 'app-licitacoes-detalhar-contratos',
@@ -22,72 +16,16 @@ export class LicitacoesDetalharContratosComponent implements OnInit {
 
   public contratosLicitacao$: Observable<ContratoLicitacao[]>;
 
-  public itemSelecionado: ItensContrato;
-  public activeIds: string[] = [];
-  public radioGroupForm: FormGroup;
-  public showTotal = false;
-
   constructor(
     private activatedroute: ActivatedRoute,
-    private formBuilder: FormBuilder,
-    private modalService: NgbModal,
-    private licitacaoService: LicitacaoService,
-    private itensService: ItensService) { }
+    private licitacaoService: LicitacaoService) { }
 
   ngOnInit() {
-    this.activatedroute.parent.params.pipe(take(1)).subscribe(params => {
-      this.getContratosLicitacaoByID(params.id);
-    });
-    this.activatedroute.queryParams.pipe(take(1)).subscribe(params => {
-      this.activeIds = ['panel-' + params.id];
-    });
-    this.radioGroupForm = this.formBuilder.group({
-      showTotal: false
-    });
-  }
-
-  getContratosLicitacaoByID(id: string) {
-    this.contratosLicitacao$ = this.licitacaoService.getContratos(id);
-  }
-
-  getMediaItensSemelhantes(dsItem: string[], dataInicioContrato: Date) {
-    const termos = [dsItem[0], dsItem.slice(0, 2).join(' & '), dsItem.join(' & ')];
-    return this.itensService.getItensSimilares(termos, dataInicioContrato)
-      .pipe(take(1),
-        map(itens => {
-          const itensOrdenados = itens.slice(0, 21).sort((a, b) => a.vl_item_contrato - b.vl_item_contrato);
-          const meioInf = Math.floor((itensOrdenados.length - 1) / 2);
-          const meioSup = Math.ceil((itensOrdenados.length - 1) / 2);
-          const mediana = (itensOrdenados[meioInf].vl_item_contrato + itensOrdenados[meioSup].vl_item_contrato) / 2;
-          return { mediana, itensOrdenados };
-        })
-      ).toPromise();
-  }
-
-  getTipoFornecedor(tipoFornecedor: string): string {
-    if (tipoFornecedor === 'J') {
-      return 'Pessoa Jurídica';
-    } else if (tipoFornecedor === 'F') {
-      return 'Pessoa Física';
-    } else if (tipoFornecedor === 'C') {
-      return 'Consórcio';
-    }
-    return 'Outros';
-  }
-
-  defineCorFundo(valor: number): string {
-    const cor: any = d3.scaleLinear()
-    .domain([-1, 0, 1])
-    .range(['#72a5b6', '#ffffff', '#d7856c']);
-    return cor(valor);
-  }
-
-  defineCor(valor: number): string {
-    return (valor >= 1 || valor <= -1) ? 'white' : 'black';
-  }
-
-  open(content, item: ItensContrato): void {
-    this.itemSelecionado = item;
-    this.modalService.open(content, { ariaLabelledBy: 'modal-descricao', size: 'xl' });
+    this.contratosLicitacao$ = this.activatedroute.parent.params
+      .pipe(
+        take(1),
+        concatMap(params => this.licitacaoService.getContratos(params.id)
+        )
+      );
   }
 }
--- server/routes/api/contratos.js
+++ server/routes/api/contratos.js
@@ -63,6 +63,29 @@ router.get("/vigentes", (req, res) => {
     .catch(err => res.status(BAD_REQUEST).json({ err }));
 });
 
+router.get("/licitacao/:id", (req, res) => {
+  Contrato.findAll({
+    raw: true,
+    attributes: {
+      include: [[Sequelize.col('contratoFornecedor.nm_pessoa'), 'nm_fornecedor'],
+                [Sequelize.col('contratoFornecedor.tp_pessoa'), 'tp_fornecedor'],
+                "id_contrato", "id_licitacao", "nr_contrato", "nr_documento_contratado", "vl_contrato", "dt_inicio_vigencia", "dt_final_vigencia"]
+    },
+    include: [
+      {
+        model: Fornecedor,
+        attributes: [],
+        as: "contratoFornecedor"
+      }
+    ],
+    where: {
+      id_licitacao: req.params.id
+    }
+  })
+  .then(contratos => res.status(SUCCESS).json(contratos))
+  .catch(err => res.status(BAD_REQUEST).json({ err }));
+});
+
 router.get("/:id", (req, res) => {
   Contrato.findOne({
     include: [
@@ -104,94 +127,6 @@ router.get("/:id", (req, res) => {
       console.log(err)
       res.status(BAD_REQUEST).json({ err })
     });
-});
-
-router.get("/licitacao/:id", (req, res) => {
-  Contrato.findAll({
-    attributes: ["id_contrato", "nr_contrato", "nr_documento_contratado", "vl_contrato", "dt_inicio_vigencia", "dt_final_vigencia", "ano_contrato"],
-    include: [
-      {
-        model: Fornecedor,
-        attributes: ["nm_pessoa", "tp_pessoa"],
-        as: "contratoFornecedor"
-      },
-      {
-        model: itensContrato,
-        include: [
-          {
-            model: itensLicitacao,
-            attributes: ["vl_unitario_estimado", "sg_unidade_medida"],
-            as: "itensLicitacaoItensContrato"
-          }
-        ],
-        attributes: ["qt_itens_contrato", "vl_item_contrato", "vl_total_item_contrato", "ds_item", "categoria", "ano_licitacao", "dt_inicio_vigencia"],
-        as: "itensContrato"
-      },
-      {
-        model: Novidade,
-        attributes: ["id_tipo", [sequelize.literal('CAST(texto_novidade as REAL)'), 'valor']],
-        required: false,
-        as: "contratoNovidade",
-        where: {
-          id_tipo: {
-            [Op.or]: [6, 9] // 6 (novidade de pagamento) e 9 (novidade de empenho)
-          }
-        }
-      }
-    ],
-    where: {
-      id_licitacao: req.params.id
-    }
-  })
-    .then(contratos => {
-
-      contratos = contratos.map(contrato => {
-        let data = contrato.toJSON();
-
-        if (data.contratoNovidade && data.contratoNovidade.length > 0) {
-          data.total_pago = data.contratoNovidade
-            .map(item => item.valor)
-            .reduce((prev, next) => prev + next);
-        } else {
-          data.total_pago = 0;
-        }
-
-        return data;
-      })
-
-      res.json(contratos)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(BAD_REQUEST).json({ err })
-    });
-});
-
-router.get("/licitacao/:id/fornecedores", (req, res) => {
-  Fornecedor.findAll({
-    attributes: ["nr_documento", "nm_pessoa", "tp_pessoa"],
-    include: [
-      {
-        attributes: ["nr_contrato", "ano_contrato", "vl_contrato", "id_contrato"],
-        model: Contrato,
-        as: "fornecedorContratos",
-        where: {
-          id_licitacao: req.params.id
-        }
-      }
-    ],
-  })
-    .then(fornecedores => {
-      fornecedores = fornecedores.map(f => f.get({ plain: true }));
-
-      fornecedores.forEach((value) => {
-        value.total_contratado = value.fornecedorContratos
-          .reduce((a, b) => a + (b["vl_contrato"] || 0), 0);
-      });
-
-      res.status(SUCCESS).json(fornecedores)
-    })
-    .catch(err => res.status(BAD_REQUEST).json({ err: err.message }));
 });
 
 module.exports = router;
