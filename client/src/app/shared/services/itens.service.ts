90d0cba9b92ed22f0528aef7c8706cc688598a92
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-07-23 10:35:23 -0300

    Adiciona filtro de unidade do item

--- client/src/app/shared/services/itens.service.ts
+++ client/src/app/shared/services/itens.service.ts
@@ -8,38 +8,38 @@ import { ItensContrato } from '../models/itensContrato.model';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ItensService {
 
-    private url = environment.apiUrl + 'itensContrato';
+  private url = environment.apiUrl + 'itensContrato';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    get(idItem: string): Observable<ItensContrato> {
-      return this.http.get<ItensContrato>(`${this.url}/item/${idItem}`);
-    }
+  get(idItem: string): Observable<ItensContrato> {
+    return this.http.get<ItensContrato>(`${this.url}/item/${idItem}`);
+  }
 
-    getByContrato(idContrato: string): Observable<ItensContrato[]> {
-      return this.http.get<ItensContrato[]>(`${this.url}/contrato/${idContrato}`);
-    }
+  getByContrato(idContrato: string): Observable<ItensContrato[]> {
+    return this.http.get<ItensContrato[]>(`${this.url}/contrato/${idContrato}`);
+  }
 
-    getItensSimilares(termos: string[], dataInicioContrato: Date): Observable<ItensContrato[]> {
-        return this.http.post<ItensContrato[]>(this.url + '/similares', { termo: termos, data: dataInicioContrato });
-    }
+  getItensSimilares(item: ItensContrato, termos: string[]): Observable<ItensContrato[]> {
+      return this.http.post<ItensContrato[]>(this.url + '/similares', { termo: termos, data: item.dt_inicio_vigencia });
+  }
 
-    getMediaItensSemelhantes(item: ItensContrato, termos: string[]): Observable<ItensContrato> {
-      return this.getItensSimilares(termos, item.dt_inicio_vigencia)
-        .pipe(take(1),
-          map(itens => {
-            const itensOrdenados = itens.sort((a, b) => a.vl_item_contrato - b.vl_item_contrato);
-            const meioInf = Math.floor((itensOrdenados.length - 1) / 2);
-            const meioSup = Math.ceil((itensOrdenados.length - 1) / 2);
-            const mediana = (itensOrdenados[meioInf].vl_item_contrato + itensOrdenados[meioSup].vl_item_contrato) / 2;
-            item.mediana_valor = mediana;
-            item.itensSemelhantes = itensOrdenados;
-            return item;
-          })
-        );
-    }
+  getMediaItensSemelhantes(item: ItensContrato, termos: string[]): Observable<ItensContrato> {
+    return this.getItensSimilares(item, termos)
+      .pipe(take(1),
+        map(itens => {
+          const itensOrdenados = itens.sort((a, b) => a.vl_item_contrato - b.vl_item_contrato);
+          const meioInf = Math.floor((itensOrdenados.length - 1) / 2);
+          const meioSup = Math.ceil((itensOrdenados.length - 1) / 2);
+          const mediana = (itensOrdenados[meioInf].vl_item_contrato + itensOrdenados[meioSup].vl_item_contrato) / 2;
+          item.mediana_valor = mediana;
+          item.itensSemelhantes = itensOrdenados;
+          return item;
+        })
+      );
+  }
 }
