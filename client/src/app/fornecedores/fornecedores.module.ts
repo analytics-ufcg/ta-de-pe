81418290cdda0b677f9ee88fc848bc4ae2644cc0
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2020-08-12 17:32:57 -0300

    Cria tela de detalhes de fornecedor, listando detalhes e contratos (390)

--- client/src/app/fornecedores/fornecedores.module.ts
+++ client/src/app/fornecedores/fornecedores.module.ts
@@ -1,15 +1,25 @@
-import { FornecedoresRoutingModule } from './fornecedores-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from './../shared/components/shared.module';
+import { PipesModule } from './../shared/pipes/pipes.module';
+import { FornecedoresRoutingModule } from './fornecedores-routing.module';
+
 import { InfoFornecedorComponent } from './info-fornecedor/info-fornecedor.component';
 
 
 
 @NgModule({
-  declarations: [InfoFornecedorComponent],
+  declarations: [
+    InfoFornecedorComponent],
   imports: [
     CommonModule,
-    FornecedoresRoutingModule
+    FormsModule,
+    NgbModule,
+    FornecedoresRoutingModule,
+    SharedModule,
+    PipesModule
   ]
 })
 export class FornecedoresModule { }
--- client/src/app/fornecedores/info-fornecedor/info-fornecedor.component.ts
+++ client/src/app/fornecedores/info-fornecedor/info-fornecedor.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { EventoOrd } from './../../shared/models/lista.model';
+import { OrdenavelDirective } from 'src/app/shared/directives/ordenavel.directive';
+import { take, takeUntil, map } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Component, OnInit, ViewChildren, QueryList, SimpleChanges } from '@angular/core';
+
+import { Fornecedor } from './../../shared/models/fornecedor.model';
+import { ListaContratosFornecedorService } from './../../shared/services/lista.service';
+import { FornecedorService } from './../../shared/services/fornecedor.service';
 
 @Component({
   selector: 'app-info-fornecedor',
   templateUrl: './info-fornecedor.component.html',
-  styleUrls: ['./info-fornecedor.component.scss']
+  styleUrls: ['./info-fornecedor.component.scss'],
+  providers: [ListaContratosFornecedorService, DecimalPipe]
 })
 export class InfoFornecedorComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe = new Subject();
+  
+  @ViewChildren(OrdenavelDirective) cabecalhos: QueryList<OrdenavelDirective>;
 
-  ngOnInit() {
+  public fornecedor: Fornecedor;
+  public isLoading = true;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fornecedorService: FornecedorService,
+    public listaService: ListaContratosFornecedorService
+  ) {
+   }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(take(1)).subscribe(params => {
+      this.getFornecedorByID(params.id)
+      this.getContratosFornecedorByID(params.id)
+    });
+  }
+
+
+  getFornecedorByID(id: string) {
+    this.fornecedorService
+      .get(id)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(fornecedor => {
+        this.fornecedor = fornecedor;
+      });
+  }
+
+  getContratosFornecedorByID(id: string) {
+    this.listaService.dados$ = this.fornecedorService
+      .getContratos(id)
+      .pipe(map(contratos => {
+        // itens ordenados pelo preço
+        contratos.sort((c1, c2) => new Date(c1.dt_inicio_vigencia).getTime() - new Date(c2.dt_inicio_vigencia).getTime());
+        return contratos;
+      }))
+  }
+ 
+  onOrdenar({coluna, direcao}: EventoOrd) {
+    // Reseta outros cabeçalhos
+    this.cabecalhos.forEach(cab => {
+      if (cab.ordenavel !== coluna) {
+        cab.direcao = '';
+        cab.ordAsc = false;
+        cab.ordDesc = false;
+      }
+    });
+
+    this.listaService.colunaOrd = coluna;
+    this.listaService.direcaoOrd = direcao;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
