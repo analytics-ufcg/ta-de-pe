d5c5cd2fec7c57a6d6e938efabb277d7a0eee810
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2021-02-03 12:20:44 -0300

    Readiciona endpoint que retorna todos os municípios

--- server/routes/api/orgaos.js
+++ server/routes/api/orgaos.js
@@ -17,6 +17,25 @@ router.get("/", (req, res) => {
 });
 
 router.get("/municipios", (req, res) => {
+  Orgaos.findAll({
+    attributes: [
+      [
+        Sequelize.fn("DISTINCT", Sequelize.col("nome_municipio")),
+        "nome_municipio"
+      ],
+      "sigla_estado"
+    ],
+    where: {
+      nome_municipio: {
+          [Op.ne]: null
+      }
+    }
+  })
+    .then(municipios => res.status(SUCCESS).json(municipios))
+    .catch(err => res.status(BAD_REQUEST).json({ err }));
+});
+
+router.get("/municipiosBusca", (req, res) => {
   const termo = req.query.termo;
 
   Orgaos.findAll({
