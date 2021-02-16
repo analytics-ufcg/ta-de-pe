1c0b8b93470d4efd24474ad8c77a9b520b4140fb
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2021-02-16 14:45:26 -0300

    Altera tamanho da descrição do item

--- client/src/app/shared/pipes/resumir-texto.pipe.ts
+++ client/src/app/shared/pipes/resumir-texto.pipe.ts
@@ -9,9 +9,9 @@ export class ResumirTextoPipe implements PipeTransform {
     let tamanho = 20;
     // muda de acordo com o tamanho da tela
     if (window.innerWidth >= 1500) {
-      tamanho = window.innerWidth / 65;
-    } else if (window.innerWidth >= 500) {
       tamanho = window.innerWidth / 55;
+    } else if (window.innerWidth >= 500) {
+      tamanho = window.innerWidth / 25;
     }
     const split = descricao.split(/\s+|:|,/);
     let str = '';
