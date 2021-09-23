90175832d4a268774e4a5329b3dc4d3e1a3f18a6
Jefferson Neves <jefferson.rpn@gmail.com>
2021-09-23 12:20:24 -0300

    Resolve substituição de arquivos estátivos (faq)

---
+++ client/src/environments/covid/faq.data.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+export const faqsData = [
+ {
+   titulo: `O que significam as comparações de preço do Tá de Pé?`,
+   texto: `As comparações têm por objetivo mostrar se a gestão realizou uma compra relativamente econômica ou não. Há dois indicadores de comparação de preço: com demais municípios e com o valor estimado.`
+ },
+ {
+   titulo: `O que indica a \"diferença com estado\" na tabela de itens contratados?`,
+   texto: `A diferença com estado é uma comparação entre o preço contratado do item (valor por unidade) e o que foi pago em outros contratos (mediana no estado). Assim, a coluna aponta quanto determinada prefeitura pagou a mais ou a menos que a mediana, considerando contratos de municípios do estado que licitaram produtos iguais ou semelhantes dentro de um período de seis meses antes ou depois da contratação. É um indicador se o município economizou ou desperdiçou recursos, tendo em vista o que foi pago em outros contratos. A comparação é feita de forma automatizada pelo texto dos objetos licitados informado pela prefeitura. Por isso, ao fazer uma comparação entre itens específicos, é recomendável checar sua descrição completa.`
+ },
+ {
+   titulo: `O que indica a \"diferença com estimado\" na tabela de itens contratados?`,
+   texto: `Trata-se de uma comparação entre valor estimado e contratado, sendo assim um outro indicador de economia. Na abertura do edital de licitação a prefeitura aponta um valor de referência que está sendo pago pelo mercado, o valor estimado. O índice revela a diferença entre quanto se esperava gastar e quanto efetivamente se gastou com determinado produto. Como é possível que algumas prefeituras inflem o valor estimado, a comparação deste índice com o de valores pagos no estado oferece uma análise conjunta mais robusta.`
+ },
+ {
+   titulo: `Como a Transparência Brasil encontra estes dados?`,
+   texto: `Todos os dados usados pelo Tá de Pé são dados públicos de fontes oficiais. A Transparência Brasil coleta dados em portais dos Tribunais de Contas e por meio da Lei de Acesso à Informação, e apresenta suas próprias análises na plataforma Tá de Pé Merenda. Caso encontre alguma inconsistência nas informações, por favor entre em contato por e-mail no endereço tadepe@transparencia.org.br`
+ },
+ {
+   titulo: `Não encontro o meu município no site Tá de Pé, o que devo fazer?`,
+   texto: `Nesta primeira fase do projeto, estamos divulgando apenas os contratos, licitações e empenhos dos municípios do Rio Grande do Sul e de Pernambuco, cujos tribunais de contas estaduais publicam dados com boa qualidade, especificamente o preço por item contratado, que permite realizar análises comparativas. Infelizmente, esta não é a realidade dos dados de contratação publicados pelos demais estados. Ainda assim apresentaremos em breve análises com dados de outros estados. Precisaremos do apoio de indivíduos para abrir essas informações. Se você tem interesse em ajudar para que possamos oferecer a análise, entre em contato por e-mail no endereço tadepe@transparencia.org.br, informando seu município e estado.`
+ },
+ {
+   titulo: `O que é a Malha Fina da Transparência Brasil?`,
+   texto: `A Malha Fina é uma seleção de compras em que detectamos algo incomum. Podem ser produtos fora da área de atuação da empresa vendedora ou empresas fundadas logo antes da contratação com o poder público. A seleção é feita de forma automática com base no cadastro das empresas na Receita Federal e como elas definem suas atividades segundo a Classificação Nacional de Atividades Econômicas. A Malha Fina facilita a identificação de compras públicas atípicas, mas não é capaz de indicar, por si só, irregularidades, cabendo ao cidadão explorar o contexto das informações fornecidas.`
+ }
+];
