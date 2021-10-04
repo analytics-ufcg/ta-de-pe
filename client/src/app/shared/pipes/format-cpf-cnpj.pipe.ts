1dae6702e29c9093ffed01e3ce9579c130d3627d
Jefferson Neves <jefferson.rpn@gmail.com>
2021-10-04 11:57:34 -0300

    Add regra de formatação de CPF de 6 dígitos para o pipe de formatCpfCnpj

--- client/src/app/shared/pipes/format-cpf-cnpj.pipe.ts
+++ client/src/app/shared/pipes/format-cpf-cnpj.pipe.ts
@@ -8,11 +8,14 @@ export class FormatCpfCnpjPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     if (!value) { return value; }
     const cnpjCpf = value.replace(/\D/g, '');
+    const tipoPessoa = args[0];
 
     if (cnpjCpf.length === 11) {
       return cnpjCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '\$1.\$2.\$3-\$4');
     } else if (cnpjCpf.length === 14) {
       return cnpjCpf.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g, '\$1.\$2.\$3/\$4-\$5');
+    } else if (cnpjCpf.length === 6 && tipoPessoa === 'F') {
+      return cnpjCpf.replace(/(\d{3})(\d{3})/g, '***.\$1.\$2-**');
     }
 
     return cnpjCpf;
